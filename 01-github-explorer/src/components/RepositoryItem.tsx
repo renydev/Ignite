692c56da4c5cea9e4004b84898487690286a71ac
Renato Ventura <re.infotec@gmail.com>
2021-05-22 21:20:56 -0300

    Convertendo para Typescript

--- 01-github-explorer/src/components/RepositoryItem.tsx
+++ 01-github-explorer/src/components/RepositoryItem.tsx
@@ -1,5 +1,12 @@
 // const repositoryName = 'unform2';
-export function RepositoryItem(props) {
+interface RepositoryItemProps {
+  repository: {
+    name: string
+    description: string
+    html_url: string
+  }
+}
+export function RepositoryItem(props: RepositoryItemProps) {
   return (
     <li>
       <strong>{props.repository?.name ?? 'default'}</strong>
