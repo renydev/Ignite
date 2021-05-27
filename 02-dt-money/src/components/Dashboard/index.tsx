d6df955969cce404e69d7f1ca87157fc14f50966
Renato Ventura <re.infotec@gmail.com>
2021-05-27 05:41:11 -0300

    Tabela de transações

--- 02-dt-money/src/components/Dashboard/index.tsx
+++ 02-dt-money/src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Container } from "../Dashboard/styles";
 import { Summary } from "../Summary";
+import { TransationsTable } from "../TransationsTable";
 
 export function Dashboard() {
   return (
     <Container>
       <Summary/>
+      <TransationsTable />
     </Container>
   )
 }
