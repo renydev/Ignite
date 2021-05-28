a20af8dc0ebbf6c6ede951a443139f5c2dd3baed
Renato Ventura <re.infotec@gmail.com>
2021-05-27 23:33:20 -0300

    Estilizando modal

--- 02-dt-money/src/components/NewTransactionModal/index.tsx
+++ 02-dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from "react-modal";
+import { Container } from "./styles";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -10,8 +11,20 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
   return (
     <Modal
       isOpen={isOpen}
-      onRequestClose={onRequestClose}>
-      <h2>Cadastrar Transação</h2>
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <Container>
+        <h2>Cadastrar Transação</h2>
+
+        <input type="text" placeholder="Título" />
+        <input type="number" placeholder="Valor" />
+        <input type="text" placeholder="Categoria" />
+        <button type="submit">Enviar</button>
+
+      </Container>
+
     </Modal>
   )
 }
