86fc0bc1cfe4241c73af6c0e6ed3ba03b625b0fb
Renato Ventura <re.infotec@gmail.com>
2021-05-29 22:14:15 -0300

    Styled Components
    Styled Components, criando propriedades de componentes e interpolando com o arquivo de estilos

--- 02-dt-money/src/components/NewTransactionModal/index.tsx
+++ 02-dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
 import Modal from "react-modal";
-import { Container, TransactionTypeContainer } from "./styles";
+import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
+import { useState } from "react";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -10,6 +11,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+
+  const [type, setType] = useState('deposit');
 
   return (
     <Modal
@@ -30,14 +33,20 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
         <input type="text" placeholder="Título" />
         <input type="number" placeholder="Valor" />
         <TransactionTypeContainer>
-          <button type="button">
+          <RadioBox
+            type="button"
+            isActive={type === 'deposit'}
+            onClick={() => setType('deposit')}>
             <img src={incomeImg} alt="" />
             <span>Entrada</span>
-          </button>
-          <button type="button">
+          </RadioBox>
+          <RadioBox
+            type="button"
+            isActive={type === 'withdraw'}
+            onClick={() => setType('withdraw')}>
             <img src={outcomeImg} alt="" />
             <span>Saída</span>
-          </button>
+          </RadioBox>
         </TransactionTypeContainer>
         <input type="text" placeholder="Categoria" />
         <button type="submit">Enviar</button>
