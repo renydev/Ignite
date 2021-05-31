618974c9499192156054217a3f62092c1d41296e
Renato Ventura <re.infotec@gmail.com>
2021-05-31 05:31:25 -0300

    Usando transactions Schema
    Cadastrando informações no mirage e mostrando o retorno no console log

--- 02-dt-money/src/App.tsx
+++ 02-dt-money/src/App.tsx
@@ -1,9 +1,8 @@
+import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-import { GlobalStyle } from "./styles/global";
-import { useState } from "react";
-import Modal from "react-modal";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { GlobalStyle } from "./styles/global";
 
 
 export function App() {
